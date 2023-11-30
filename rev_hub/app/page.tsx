2c97c62f4d2a21439283ac7417abcdf28f9c42dc
velia-f <cognomeenome870@gmail.com>
2023-11-30 10:55:12 +0100

    nulla

--- rev_hub/app/page.tsx
+++ rev_hub/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
         <main className="flex flex-col min-h-screen items-center p-8">
             <h1>RevHub</h1>
             <p>ciao</p>
+            <p>prova_1_0</p>
         </main>
     )
 }
