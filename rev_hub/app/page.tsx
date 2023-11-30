fb65ee15a3e186f8cd5d2686f73dc1345685df0b
Scotonax <alessandro.scotton.03@gmail.com>
2023-11-30 09:45:16 +0100

    aggiunta base per il codice next dell'applicazione

---
+++ rev_hub/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+    return (
+        <main className="flex flex-col min-h-screen items-center p-8">
+            <h1>RevHub</h1>
+            <p>ciao</p>
+        </main>
+    )
+}
