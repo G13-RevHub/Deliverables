d2afe634a6ab0be0d56101b88b5cdbeed5b723f4
velia-f <cognomeenome870@gmail.com>
2023-12-09 17:52:10 +0100

    components style

--- rev_hub/components/footer.tsx
+++ rev_hub/components/footer.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 
 
 export default function Footer() {
     return (
         <div className="bg-black text-grey text-[17px] flex justify-center items-center bottom-0 w-full h-20">
-            <p>Tutti i diritti sono riservati &copy;2023-2024</p>
+            <p className="center">Tutti i diritti sono riservati &copy;2023-2024 <br />
+            <Link href="mailto:cognomeenome870@gmail.com">Se voleste contattarci</Link>
+            </p>
         </div>
     )
 }
