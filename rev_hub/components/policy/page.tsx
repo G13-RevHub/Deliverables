014c1283ef42bf6b4d5aef0b0f1f7dcbfd68496c
velia-f <cognomeenome870@gmail.com>
2023-12-09 17:40:33 +0100

    policy

--- rev_hub/components/policy/page.tsx
+++ rev_hub/components/policy/page.tsx
@@ -1,6 +1,14 @@
+import Head from "next/head";
+
 export default function Policy() {
     return (
-        <main>
+        <html lang="en">
+            <Head>
+                <title>Privacy Policy - RevHub</title>
+                <meta name='description'>Applicazione sviluppata dal gruppo G13</meta>
+            </Head>
+            <body className="min-h-screen flex flex-col justify-between">
+                
             <h1>Privacy Policy</h1>
 <p>Last updated: December 09, 2023</p>
 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
@@ -145,6 +153,7 @@ export default function Policy() {
 <ul>
 <li>By email: cognomeenome870@gmail.com</li>
 </ul>
-        </main>
+            </body>
+        </html>
     )
 }
