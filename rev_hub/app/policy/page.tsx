e36ae367957ab577279e3b2cc67d804e24fd3c58
velia-f <cognomeenome870@gmail.com>
2023-12-09 17:57:07 +0100

    final policy

--- rev_hub/app/policy/page.tsx
+++ rev_hub/app/policy/page.tsx
@@ -4,14 +4,8 @@ import Head from "next/head";
 
 export default function Policy() {
     return (
-        <html lang="en">
-            <Head>
-                <title>Privacy Policy - RevHub</title>
-                <meta name='description'>Applicazione sviluppata dal gruppo G13</meta>
-            </Head>
-            <body className="min-h-screen flex flex-col justify-between">
-                
-            <h1>Privacy Policy</h1>
+        <main>
+<h1>Privacy Policy</h1>
 <p>Last updated: December 09, 2023</p>
 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
 <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/" target="_blank">Free Privacy Policy Generator</a>.</p>
@@ -60,15 +54,15 @@ export default function Policy() {
 <h4>Personal Data</h4>
 <p>While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to:</p>
 <ul>
-<li>
-<p>Email address</p>
-</li>
-<li>
-<p>First name and last name</p>
-</li>
-<li>
-<p>Usage Data</p>
-</li>
+    <li>
+    <p>Email address</p>
+    </li>
+    <li>
+    <p>First name and last name</p>
+    </li>
+    <li>
+    <p>Usage Data</p>
+    </li>
 </ul>
 <h4>Usage Data</h4>
 <p>Usage Data is collected automatically when using the Service.</p>
@@ -155,7 +149,6 @@ export default function Policy() {
 <ul>
 <li>By email: cognomeenome870@gmail.com</li>
 </ul>
-            </body>
-        </html>
+        </main>
     )
 }
