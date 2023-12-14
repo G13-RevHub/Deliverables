ffdaafba23874f20bcbb9a493dbdef2f0f18f40f
velia-f <cognomeenome870@gmail.com>
2023-12-14 11:32:41 +0100

    migliorato search

--- rev_hub/app/search/[type]/[text]/page.tsx
+++ rev_hub/app/search/[type]/[text]/page.tsx
@@ -6,27 +6,36 @@ import { useEffect, useState } from "react"
 import React from 'react';
 
 function createList(tag: string, results: any[]) {
+    console.log("Results_2: ", results)
+    if (!results || results.length === 0) {
+        return <div>Undefined.</div>;
+    }
     if (tag === "review" || tag === "tag") {
         return (
             <div>
+                <p>pin</p>
                 {
-                    results.map((title, id) => {
+                    //non so perché ci vadano le () al posto di {} dopo =>
+                    results.map((title, id) => (
                         <Link key={id} href={`/review/${id}`}>{title}</Link>
-                    })
+                    ))
                 }
             </div>
         )
     } else if (tag === "user") {
         return (
             <div>
+                <ul>
                 {
-                    results.map((id, username) => {
-                        <Link key={id} href={`/profile/${id}`}>{username}</Link>
-                    })
+                    results.map((id, username) => (
+                        <li><Link key={id} href={`/profile/${id}`}>{username}</Link></li>
+                    ))
                 }
+                </ul>
             </div>
         )
     }
+    return <div>Nessun risultato disponibile.</div>;
 }
 
 export default function SearchReview({ params }: { params: { type: string, text: string } }) {
@@ -36,13 +45,15 @@ export default function SearchReview({ params }: { params: { type: string, text:
     const txt = params.text.replaceAll('@', '').replaceAll('#', '').replaceAll("%20", " ").replaceAll("%22", '"')
 
     useEffect(() => {
+        console.log("Results: ", results)
         if (txt.length === 0) return
 
         console.log(`searching ${txt} using ${params.type}`)
 
         axios.get(`/api/search/${params.type}/${txt}`).then( res => {
-            console.log(res.data)
-            setResults(res.data)
+            console.log("res.data: " + res.data)
+            console.log("res.data.items: " + res.data.items)
+            setResults(res.data.items)//forse qui va .items perché se no dà errore con results.map
             setLoading(false)
         }).catch(error => {
             alert("Error while searching: " + error.message)
@@ -55,15 +66,18 @@ export default function SearchReview({ params }: { params: { type: string, text:
         return (
             <main className="flex flex-col items-center p-8">
                 <h1>RevHub</h1>
-                <h2>LOADING...</h2>
+                <h2>Caricamento...</h2>
+                <h4>In caso di lentezza, la preghiamo di ricaricare</h4>
             </main>
         )
     else
         return (
             <main className="flex flex-col p-8 w-full">
-                <h1 className="text-[25px] font-bold">Search Review for "{params.type}"</h1>
+                <h1 className="text-[25px] font-bold">Search Review for "{params.text}"</h1>
                 <div className="flex space-x-2">
-                    {/* {createList(tag, results)} */}
+                    {
+                        createList(params.type, results) 
+                    }
                 </div>
             </main>
         )
