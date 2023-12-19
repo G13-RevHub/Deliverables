3999f733aea6784b877ee13c3edc4225f2284388
3le4ms1 <ismaeledegiorgi@gmail.com>
2023-12-19 17:05:16 +0100

    Visualizzazione delle recensioni più viste

---
+++ rev_hub/app/api/review/getPerViews/route.ts
@@ -0,0 +1,21 @@
+import { connectDB } from "@/configs/dbConfig";
+import Review  from "@/models/reviewModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function GET(request: NextRequest, { params }: { params: { text: string } }) {
+    try {
+        const result = await Review.find({}).sort({ views: -1 })
+
+        if(!result || result.length === 0) {
+            return NextResponse.json({results: []})
+        }
+        return NextResponse.json({ results: result })
+    } catch (error: any) {
+        return NextResponse.json(
+            { message: error.message },
+            { status: 400 }
+        )
+    }
+}
--- rev_hub/app/page.tsx
+++ rev_hub/app/page.tsx
@@ -2,39 +2,48 @@
 
 import Link from "next/link"
 import { useEffect, useState } from "react"
+import axios from 'axios'
 
 export default function Home() {
     const [loading, setLoading] = useState(true)
-
-    const lista = [1, 2, 3, 4, 5]
+    const [reviews, setReviews] = useState<any[]>([])
 
     useEffect(() => {
-        if (loading) return
-
-
+        if (loading) {
+            axios.get("/api/review/getPerViews").then( res => {
+                setReviews(res.data.results)
+            })
+            setLoading(false)
+        }
     }, [])
 
-    useEffect(() => {
-        //esegue la funzione passatagli come parametro allo scadere del timeout, che in questo caso è di 2000 ms
-        setTimeout(() => {
-            setLoading(false)
-        }, 1000)
-    })
 
-    if (loading)
+    /* useEffect(() => {
+     *     //esegue la funzione passatagli come parametro allo scadere del timeout, che in questo caso è di 2000 ms
+     *     setTimeout(() => {
+     *         setLoading(false)
+     *     }, 1000)
+     * }) */
+
+    if (loading) {
         return (
             <main className="flex flex-col items-center p-8">
                 <h1>RevHub</h1>
                 <h2>LOADING...</h2>
             </main>
         )
-    else
+    } else {
+        console.log(typeof(reviews))
+        console.log(reviews)
         return (
             <main className="flex flex-col p-8">
                 <h1 className="text-[25px] font-bold">Recensioni più viste</h1>
-                {lista.map((value, id) => (
-                    <Link key={id} href={`/review/${value}`}>recensione {value}</Link>
-                ))}
+
+            {reviews.map((rev, id) => (
+                <Link key={id} href={`/review/${rev.id}`}>{rev.title}</Link>
+            ))}
+
             </main>
         )
+    }
 }
